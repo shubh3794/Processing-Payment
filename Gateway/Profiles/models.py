81c0223889a450f4d023d91eb2890e385afd198b
shubham <shubh.aggarwal.37@gmail.com>
2015-11-26 02:26:41 +0530

    creating new stripe_id for customers as they try to pay through their cards

--- Gateway/Profiles/models.py
+++ Gateway/Profiles/models.py
@@ -1,6 +1,9 @@
 from django.conf import settings
 from django.db import models
 from allauth.account.signals import user_logged_in, user_signed_up
+import stripe
+# Create your views here.
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 # Create your models here.
 class Profiles(models.Model):
@@ -23,10 +26,15 @@ def clbk(sender,request,user,**kwargs):
 	stripe_ac,created = userStripe.objects.get_or_create(user=user)
 	if created:
 		print "created for %s"%(user.username)
+
+	if stripe_ac.stripe_id == None or stripe_ac.stripe_id == "":
+		new_stripe_id = stripe.Customer.create(email = user.email)
+		stripe_ac.stripe_id = new_stripe_id['id']
+		stripe_ac.save()
 	prof,cr = Profiles.objects.get_or_create(user=user)
 	if cr:
 		prof.name = user.username
 		prof.save()
 
 user_logged_in.connect(clbk)
-	
+user_signed_up.connect(clbk)	
--- Gateway/checkout/views.py
+++ Gateway/checkout/views.py
@@ -7,15 +7,18 @@ stripe.api_key = settings.STRIPE_SECRET_KEY
 @login_required
 def checkout(request):
 	publishkey = settings.STRIPE_PUBLISHABLE_KEY
+	customer_id = request.user.userstripe.stripe_id
 
 	if request.method == 'POST':
 		token = request.POST['stripeToken']
 		print token
 		try:
+			customer = stripe.Customer.retrieve(customer_id)
+			customer.sources.create(source=token)
   			charge = stripe.Charge.create(
       			amount=1000, # amount in cents, again
       			currency="usd",
-      			source=token,
+      			customer = customer,
       			description="Example charge"
   			)
 		except stripe.error.CardError, e:
